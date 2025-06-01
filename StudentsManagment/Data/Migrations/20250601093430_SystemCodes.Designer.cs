6654c78a54cbf30fd3a1f0a0b51054fadd89d840
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-06-01 14:39:29 +0500

    SystemCodes Migratsiyasi berildi va Package'lar versiyasi 8.0.15 dan 8.0.16 ga ko'tarildi

--- StudentsManagment/Data/Migrations/20250601093430_SystemCodes.Designer.cs
+++ StudentsManagment/Data/Migrations/20250601093430_SystemCodes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentsManagment.Data;
 
@@ -11,9 +12,11 @@ using StudentsManagment.Data;
 namespace StudentsManagment.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250601093430_SystemCodes")]
+    partial class SystemCodes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
